2f12dd3e9ad42b5240ea755a8b305d029444c552
yrong <yrong1997@gmail.com>
2017-09-27 17:41:02 +0800

    定位修复打包文件日志级别不生效(1h)

--- index.js
+++ index.js
@@ -2,26 +2,25 @@ const path = require('path')
 const fs = require('fs')
 const log4js = require('log4js')
 const config = require('config')
-let logger_config
 
 module.exports = {
     initialize: (options) => {
+        options = options || config.get('logger')
         const logDir = options.logpath||path.join('./logs')
         if (!fs.existsSync(logDir)) {
             fs.mkdirSync(logDir)
         }
         log4js.configure(options, {cwd: logDir})
-        logger_config = options
     },
     getLogger: (name,level) => {
-        let category
+        let options = config.get('logger'),category
         try{
-            category =  name||logger_config.defaultCategory||config.get('name')
+            category =  name||options.defaultCategory||config.get('name')
         }catch(error){
             category = 'default'
         }
         logger = log4js.getLogger(category)
-        logger.setLevel(level||logger_config?logger_config.defaultLevel:'INFO')
+        logger.setLevel(level||options?options.defaultLevel:'INFO')
         return logger
     }
 }
